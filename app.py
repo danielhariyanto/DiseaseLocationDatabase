a6287de2f00ea4a6a3c2d1a5d8391c0070e2d7f8
Daniel Hariyanto <danielhariyanto@macbook-pro.dyn.brandeis.edu>
2020-08-20 20:56:12 -0400

    better UI

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # three forward slashes for relative path (no need to specify an exact location)
@@ -36,11 +35,15 @@ def index():
             return 'There was an issue adding your village'  # if there ever is a complication, this String will be returned
 
     else:
-        villages = Village.query.order_by(Village.date_created).all()  # looks at database contents in order of date created and returns all of them
         longitudespass = [i for i in db.session.query(Village.longitude)]
         latitudespass = [j for j in db.session.query(Village.latitude)]
-        return render_template('index.html', villages=villages, longitudespass=longitudespass, latitudespass=latitudespass)
+        populationpass = [k for k in db.session.query(Village.population)]
+        return render_template('index.html', longitudespass=longitudespass, latitudespass=latitudespass, populationpass=populationpass)
 
+@app.route('/database', methods=['GET'])
+def showdb():
+    villages = Village.query.order_by(Village.date_created).all()  # looks at database contents in order of date created and returns all of them
+    return render_template('database.html', villages=villages)
 
 @app.route('/delete/<int:id>')  # uses unique id to delete village
 def delete(id):
@@ -67,7 +70,7 @@ def update(id):
 
         try:
             db.session.commit()  # no need to add or delete, just commit the updated content
-            return redirect('/')  # after update, redirect back to homepage to see updated village
+            return redirect('/database')  # after update, redirect back to homepage to see updated village
         except:
             return 'There was an issue updating your village'  # if there ever is a complication, this String will be returned
 
